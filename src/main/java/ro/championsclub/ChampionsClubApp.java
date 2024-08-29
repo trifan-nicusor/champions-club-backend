61f763e9444c6c538ddabbf8e304e26663e5972c
Nicusor <nicusormadalintrifan@yahoo.com>
2024-08-29 20:33:27 +0300

    Initial commit

---
+++ src/main/java/ro/championsclub/ChampionsClubApp.java
@@ -0,0 +1,13 @@
+package ro.championsclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChampionsClubApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChampionsClubApp.class, args);
+    }
+
+}
