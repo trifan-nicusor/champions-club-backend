beebb8ec787b3f00184c39b0815c96b9409ddc66
Nicusor <nicusormadalintrifan@yahoo.com>
2024-08-30 21:34:20 +0300

    Add dtos for auth

---
+++ src/main/java/ro/championsclub/dto/request/LoginRequest.java
@@ -0,0 +1,20 @@
+package ro.championsclub.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LoginRequest {
+
+    @NotBlank
+    @Size(max = 64)
+    private String email;
+
+    @NotBlank
+    @Size(max = 64)
+    private String password;
+
+}
---
+++ src/main/java/ro/championsclub/dto/response/LoginResponse.java
@@ -0,0 +1,6 @@
+package ro.championsclub.dto.response;
+
+public record LoginResponse(String accessToken,
+                            String refreshToken) {
+
+}
