e3493532cdfada6e6bdb1f82259e84d3ad4c2d40
Nicusor <nicusormadalintrifan@yahoo.com>
2024-08-30 21:40:23 +0300

    Code formatting

--- src/main/java/ro/championsclub/dto/response/LoginResponse.java
+++ src/main/java/ro/championsclub/dto/response/LoginResponse.java
@@ -1,6 +1,5 @@
 package ro.championsclub.dto.response;
 
-public record LoginResponse(String accessToken,
-                            String refreshToken) {
+public record LoginResponse(String accessToken, String refreshToken) {
 
 }
