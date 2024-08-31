4a034b605d8ee58cb286b362be6ade6ffc04265b
Nicusor <nicusormadalintrifan@yahoo.com>
2024-08-31 17:38:12 +0300

    Remove unused method

--- src/main/java/ro/championsclub/repository/JwtTokenRepository.java
+++ src/main/java/ro/championsclub/repository/JwtTokenRepository.java
@@ -7,8 +7,6 @@ import org.springframework.transaction.annotation.Transactional;
 import ro.championsclub.entity.JwtToken;
 import ro.championsclub.entity.User;
 
-import java.util.List;
-
 public interface JwtTokenRepository extends JpaRepository<JwtToken, Long> {
 
     @Modifying
@@ -17,7 +15,5 @@ public interface JwtTokenRepository extends JpaRepository<JwtToken, Long> {
             "SET isRevoked = true, isExpired = true " +
             "WHERE user = :user")
     void deleteAllByUser(User user);
-
-    List<JwtToken> findAllByUser(User user);
 
 }
