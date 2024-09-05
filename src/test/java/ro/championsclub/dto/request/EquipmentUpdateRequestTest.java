2926a3fde62a24d2e31edd4e2abd9c2ae97c42f5
Nicusor <nicusormadalintrifan@yahoo.com>
2024-09-05 16:20:36 +0300

    Add tests for equipment dtos

---
+++ src/test/java/ro/championsclub/dto/request/EquipmentUpdateRequestTest.java
@@ -0,0 +1,46 @@
+package ro.championsclub.dto.request;
+
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class EquipmentUpdateRequestTest {
+
+    private static Validator validator;
+
+    @BeforeAll
+    public static void setup() {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        validator = factory.getValidator();
+    }
+
+    @Test
+    public void validNameTest() {
+        var request = new EquipmentUpdateRequest();
+        request.setName("name");
+
+        Set<ConstraintViolation<EquipmentUpdateRequest>> violations = validator.validate(request);
+
+        assertThat(violations).isEmpty();
+    }
+
+
+    @Test
+    public void exceedsMaxNameTest() {
+        var request = new EquipmentUpdateRequest();
+        request.setName("a".repeat(33));
+
+        Set<ConstraintViolation<EquipmentUpdateRequest>> violations = validator.validate(request);
+
+        assertThat(violations).hasSize(1);
+        assertThat(violations).extracting(ConstraintViolation::getMessage).contains("size must be between 0 and 32");
+    }
+
+}
